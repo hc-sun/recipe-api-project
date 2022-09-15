8158d5695823e56dc1c5a8171670b959dea8e0bb
n0ahsun <34866479+n0ahsun@users.noreply.github.com>
2022-09-15 20:22:18 +0800

    added postgresql adaptor

---
+++ app/app/test.py
@@ -0,0 +1,8 @@
+from django.test import SimpleTestCase
+
+
+# from app import my_test_module #module to test
+# class MyTests(SimpleTestCase):
+#     def test_my_func(self):
+#         res = module.myfunc()
+#         self.assertEqual(res, 200)
